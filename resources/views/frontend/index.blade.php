6b54d933bb4675f70d9f2f4c7a97de3539c45270
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-01-30 17:05:47 +0500

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -67,13 +67,12 @@ use Illuminate\Support\Str;
   overflow: hidden;
   min-width: 0; /* flexbox overflow fix */
   border-radius: 5px;
-    min-height: 400px;
+    min-height: 300px;
 }
 
 .hero-image img {
   width: 100%;
   height: 100%;
-  
     object-fit: cover;
   object-position: center;
   border-radius: 15px;
