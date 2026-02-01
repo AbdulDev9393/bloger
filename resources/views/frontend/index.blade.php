1236cb09b47b006e46371708cde75bb8fd044e7a
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-02-01 16:37:58 +0500

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1088,7 +1088,8 @@ use Illuminate\Support\Str;
 
   </div>
 </section>
-
+<script async="async" data-cfasync="false" src="https://pl28624175.effectivegatecpm.com/6a1c467807ba6c476df52e18a1c6588b/invoke.js"></script>
+<div id="container-6a1c467807ba6c476df52e18a1c6588b"></div>
 {{-- ================= LATEST BLOGS SECTION ================= --}}
 <section class="blogs-section">
   <div class="container">
@@ -1339,6 +1340,6 @@ use Illuminate\Support\Str;
 
 
 @include('frontend.footer')
-
+<script src="https://pl28624267.effectivegatecpm.com/76/17/9f/76179f00bab06d636434cb54393313cf.js"></script>
 </body>
 </html>
