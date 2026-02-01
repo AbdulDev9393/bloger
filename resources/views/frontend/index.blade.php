c98022b835b87d46e014a2918b03b0efd5a7d10b
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-02-01 16:56:22 +0500

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1027,7 +1027,8 @@ use Illuminate\Support\Str;
 
   </div>
 </section>
-
+<script async="async" data-cfasync="false" src="https://pl28624175.effectivegatecpm.com/6a1c467807ba6c476df52e18a1c6588b/invoke.js"></script>
+<div id="container-6a1c467807ba6c476df52e18a1c6588b"></div>
 <section class="hero">
   <div class="hero-container">
     {{-- Right Image --}}
