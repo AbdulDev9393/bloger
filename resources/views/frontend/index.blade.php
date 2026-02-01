6ce62beba4c7ddd7ab1a01d56f17a9b6fd363b85
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-02-01 16:48:54 +0500

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -981,17 +981,16 @@ use Illuminate\Support\Str;
 </style>
 <main>
  <div class="ad-center">
-    <script>
-        var atOptions1 = {
-            'key' : 'd40d0a6e881f16735879c5c25cda2f98',
-            'format' : 'iframe',
-            'height' : 90,
-            'width' : 728,
-            'params' : {}
-        };
-    </script>
-    <script src="https://www.highperformanceformat.com/d40d0a6e881f16735879c5c25cda2f98/invoke.js"></script>
-</div>
+<script>
+  atOptions = {
+    'key' : 'd40d0a6e881f16735879c5c25cda2f98',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/d40d0a6e881f16735879c5c25cda2f98/invoke.js"></script></div>
 
 <section class="hero">
   <div class="hero-container">
