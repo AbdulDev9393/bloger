75f832665cb57c7a86e17588056aac8e3efe8932
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-03-31 21:08:50 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,13 +34,13 @@ Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.log
 
 Route::get('/',[FrontendController::class,'index'])->name('frontend.index');
 Route::get('/contact-us',[FrontendController::class,'Contectus'])->name('frontend.contect');
-Route::get('/privacy-policy',[FrontendController::class,'privacyPolicy'])->name('frontend.privacy');
 Route::get('/bogs',[FrontendController::class,'bogs'])->name('frontend.blogs');
 Route::get('/bogs/seach/',[FrontendController::class,'bogs_search'])->name('frontend.search');
 Route::get('/bog-view',[FrontendController::class,'bogs__view'])->name('frontend.bogs-view');
 Route::get('/terms-condition',[FrontendController::class,'Services'])->name('frontend.terms-conditions');
 Route::get('/privacy-policy',[FrontendController::class,'condition'])->name('frontend.praivacy-policy');
 Route::get('/Aboute-us',[FrontendController::class,'Aboute'])->name('frontend.Aboute');
+Route::get('/privacy-policy',[FrontendController::class,'privacyPolicy'])->name('frontend.privacy');
 
 Route::get('/Categories/{Categories}',[FrontendController::class,'Categories'])->name('frontend.Categories');
 
