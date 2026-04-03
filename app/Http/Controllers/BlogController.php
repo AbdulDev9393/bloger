d53a8adab546c31978e1cbd4a836e9d2391da3ed
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-04-03 14:54:04 +0500

    ad

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -300,12 +300,10 @@ public function delete($id)
 
     return back()->with('success', 'Blog deleted successfully');
 }
-public function blogView($slug)
+public function blogView($id)
 {
-    $Blog_info = Blog::where('slug', $slug)->firstOrFail();
-$id = $Blog_info->id;
     $seo = BlogSeo::where('blog_id', $id)->first();
-   
+    $Blog_info = Blog::find($id);
 
     if (!$Blog_info) {
         abort(404);
