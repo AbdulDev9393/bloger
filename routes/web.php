ce02d4624e5e1f0986a70046abcc48ef1ddd56ea
AbdulDev9393 <abduldeveloper701@gmail.com>
2026-04-03 14:47:11 +0500

    ad

--- routes/web.php
+++ routes/web.php
@@ -113,7 +113,7 @@ Route::post('/sitting/media/post',[SittingController::class,'media_post'])->name
 
 });
 
-Route::get('/blog/view/{id}/{slug}', [BlogController::class, 'blogView'])->name('blogs.view');
+Route::get('/blog/{id}/{slug}', [BlogController::class, 'blogView'])->name('blogs.view');
 Route::get('/sitemap.xml', [FrontendController::class, 'sitemap']);
 
 Route::get('/sitemap_index.xml', [FrontendController::class, 'sitemap']);
