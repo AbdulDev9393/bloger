2899782078d329b4ed2b4f79f5109367b54f8095
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-06 21:13:54 -0700

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -30,12 +30,13 @@ function index() {
 public function generateAISeo(Request $request)
 {
     $blog = Blog::find($request->blog_id);
-    dd($blog);
+  
     if (!$blog) {
         return response()->json(['error' => 'Blog not found'], 404);
     }
 
     $content = strip_tags($blog->Description);
+      dd($content);
 $prompt = "
 You are a senior SEO strategist and content optimization expert working for high-ranking websites.
 
