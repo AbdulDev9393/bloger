a7e4a04f07e498a4c51d84d752f6af4d2516a982
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-06 21:02:10 -0700

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -68,7 +68,7 @@ Blog Content:
         'temperature' => 0.7,
         'max_tokens' => 1200
     ]);
-
+dd($response->json());
     $aiText = $response->json()['choices'][0]['message']['content'] ?? null;
 
     // 🧠 JSON decode safe
