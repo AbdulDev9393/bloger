d698dc1cff7b8249fd8fe6c5d0adcace8197dca1
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-21 19:05:26 -0700

    Update header.blade.php

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -94,6 +94,7 @@ $meta_schema_json = json_encode($default_schema, JSON_UNESCAPED_SLASHES|JSON_PRE
 </script>
 <script>(function(s){s.dataset.zone='10908904',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
 <!-- Custom CSS -->
+<script>(function(s){s.dataset.zone='10908907',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
 <style>
     :root {
         /* Modern Color Palette */
