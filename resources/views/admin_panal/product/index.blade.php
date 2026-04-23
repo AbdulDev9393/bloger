3f87580726bad31520c13f7a52407edb0151c028
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-22 22:29:35 -0700

    Update index.blade.php

--- resources/views/admin_panal/product/index.blade.php
+++ resources/views/admin_panal/product/index.blade.php
@@ -100,16 +100,16 @@
             </div>
 
             <!-- Pagination (Optional) -->
-            <div class="d-flex justify-content-between align-items-center mt-3">
-                <small class="text-muted">Showing 3 of 3 products</small>
-                <nav>
-                    <ul class="pagination pagination-sm mb-0">
-                        <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item disabled"><a class="page-link" href="#">Next</a></li>
-                    </ul>
-                </nav>
-            </div>
+           <div class="d-flex justify-content-between align-items-center mt-3">
+
+    <small class="text-muted">
+        Showing {{ $products->firstItem() }} to {{ $products->lastItem() }}
+        of {{ $products->total() }} products
+    </small>
+
+    {{ $products->links('pagination::bootstrap-5') }}
+
+</div>
         </div>
     </div>
 
