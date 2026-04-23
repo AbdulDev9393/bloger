17123434b7a903e6178399be9fb13c04b8a47af4
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-22 23:50:51 -0700

    Update index.blade.php

--- resources/views/frontend/product/index.blade.php
+++ resources/views/frontend/product/index.blade.php
@@ -312,13 +312,9 @@
                 <!-- PRICE (exactly uses $product->final_price ?? $product->price) -->
                 <div class="price-wrapper">
                     <span class="final-price">
-                        ${{ number_format($product->final_price ?? $product->price, 2) }}
+                     ${{ number_format($product->price, 2) }}
                     </span>
-                    @if($product->discount > 0)
-                        <span class="original-price">
-                            ${{ number_format($product->price, 2) }}
-                        </span>
-                    @endif
+                   
                 </div>
 
                 @if($product->discount > 0)
