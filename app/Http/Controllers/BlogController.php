d6998c99e34c16c5a7ad466a85dc5396e8e92ac6
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-23 06:22:33 -0700

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -606,7 +606,7 @@ public function blogView($slug)
             ];
         }
     }
-     dd($schema_array);
+     dd($breadcrumb_schema);
     return view('frontend.blogs.view', [
         'Blog_info' => $Blog_info,
         'meta_title' => $meta_title,
