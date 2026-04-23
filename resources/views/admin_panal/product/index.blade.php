64557d208d49433b6f0b3a4f2235a7fabd9c2aa0
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-04-22 23:04:13 -0700

    Update index.blade.php

--- resources/views/admin_panal/product/index.blade.php
+++ resources/views/admin_panal/product/index.blade.php
@@ -290,7 +290,14 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+<!-- Summernote CSS -->
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+
+<!-- Summernote JS -->
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 <script>
     $(document).ready(function() {
         // Initialize Summernote with full toolbar
