330df5c1922fe9fa0b23b04616e12482d4f627b8
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-05-06 10:04:20 -0700

    add

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -53,7 +53,8 @@ private function getIndexPageData()
 {
     // SEO Meta
     $meta_title = "TechBlogs Info – Tech Products 40% discount";
-    $meta_desc  = "TechBlogs.site brings you the latest Tech Products, AI updates, Products updates mobile reviews, gadgets, and digital trends.";
+    $meta_desc  = "TechBlogs.site brings you the latest Tech Products, AI updates,
+product updates, mobile reviews...";
     $meta_keywords="tech blogs, technology insights, latest tech news, AI news, artificial intelligence, AI in healthcare, AI tools 2026, software development, web development, Laravel tutorials, PHP development, programming tips, coding best practices, SEO strategies, website security, tech trends USA, mobile technology news, gadget reviews, developer guides, cloud computing, API integration, machine learning, future of AI, tech tutorials, coding for beginners, freelance development, earn money online tech, startup technology, innovation news";
     $latestBlog = Blog::where('status', 'published')->latest('id')->first();
         $products = Product::latest()->limit(6)->get();
