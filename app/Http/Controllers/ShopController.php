f3235f957ad9c0530952f7525d6e60319dd7f64b
Muhammad Abdul <abduldeveloper701@gmail.com>
2026-05-10 03:58:36 -0700

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -42,7 +42,7 @@ class ShopController extends Controller{
 
 public function uploadProduct()
 {
-   $item = Product::skip(1)->first();
+   $item = Product::all();
     $merchantId = "5784319850";
 
     $google = new GoogleMerchantService();
